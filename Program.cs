44505b56d2d242cde6fa3676fc25bf90cac78e30
Hikaru <Hikaru@DESKTOP-2OLFU7F>
2023-10-05 09:26:46 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿double media, n1, n2, n3, n4;
+
+Console.WriteLine("Digite as 4 notas do aluno:");
+n1 = Convert.ToDouble(Console.ReadLine());
+n2 = Convert.ToDouble(Console.ReadLine());
+n3 = Convert.ToDouble(Console.ReadLine());
+n4 = Convert.ToDouble(Console.ReadLine());
+
+media = n1 + n2 + n3 + n4;  
+
+if (media >= 6){
+    Console.WriteLine("Aprovado!");
+}else {
+    Console.WriteLine("Reprovado!");
+}
+
